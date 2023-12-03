b7780dcd2dd86cc7798e2cdfa94cb7e34de36a2d
Stephen Everett <alex.alexeverett@gmail.com>
2023-12-02 19:08:52 -0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use actix_web::{web::Data, App, HttpServer};
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+
+mod services;
+use services::{fetch_messages, post_message};
+
+pub struct AppState {
+    db: Pool<Postgres>
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Error building a connection pool");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(AppState { db: pool.clone() }))
+            .service(fetch_messages)
+            .service(post_message)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
