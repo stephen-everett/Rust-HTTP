01ae021ce3d594aa9cee6ae2ab038be88405a696
Stephen Everett <alex.alexeverett@gmail.com>
2023-12-03 12:55:28 -0800

    typos

--- src/services.rs
+++ src/services.rs
@@ -20,14 +20,13 @@ pub struct NewMessage {
 
 #[get("/api/get/messages")]
 pub async fn fetch_messages(state: Data<AppState>) -> impl Responder {
-    // "GET /users".to_string()
 
     match sqlx::query_as::<_, Message>("SELECT * FROM messages")
         .fetch_all(&state.db)
         .await
     {
         Ok(data) => HttpResponse::Ok().json(data),
-        Err(_) => HttpResponse::NotFound().json("No users found"),
+        Err(_) => HttpResponse::NotFound().json("No messages found"),
     }
 }
 
