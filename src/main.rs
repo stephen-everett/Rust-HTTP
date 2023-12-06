c1d4d228078062de23e1dc419241874a767bc258
Stephen Everett <alex.alexeverett@gmail.com>
2023-12-06 00:43:33 -0800

    Some endpoints to start Lobby test

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use dotenv::dotenv;
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 mod services;
-use services::{fetch_messages, post_message, test_connection, clear_messages};
+use services::{fetch_messages, post_message, test_connection, clear_messages, post_receipt, join_lobby};
 
 pub struct AppState {
     db: Pool<Postgres>
@@ -11,6 +11,11 @@ pub struct AppState {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    /*
+        Create a connection pool to the PG database using env variable "DATABASE_URL". In dev environment
+        this is defined in a .env file in the root folder of the project. In production it's defined in an
+        actual environment variable. (Don't upload .env to github. Add to .gitignore)
+     */
     dotenv().ok();
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = PgPoolOptions::new()
@@ -19,6 +24,11 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("Error building a connection pool");
 
+
+    /*
+        Start the server and use defined endpoints. Endpoints are defined in services.rs
+        and imported at the top of this file
+     */
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(AppState { db: pool.clone() }))
@@ -26,6 +36,8 @@ async fn main() -> std::io::Result<()> {
             .service(post_message)
             .service(test_connection)
             .service(clear_messages)
+            .service(post_receipt)
+            .service(join_lobby)
     })
     .bind(("0.0.0.0", 6000))?
     .run()
--- src/services.rs
+++ src/services.rs
@@ -1,11 +1,12 @@
 use actix_web::{
     get, post,
     web::{Data, Json},
-    Responder, HttpResponse
+    Responder, HttpResponse, web
 };
 use serde::{Deserialize, Serialize};
 use sqlx::{self, FromRow,};
 use crate::AppState;
+use rand::Rng;
 
 // structure for messages retrieved from DB
 #[derive(Serialize, FromRow)]
@@ -18,6 +19,26 @@ struct Message {
 #[derive(Deserialize)]
 pub struct NewMessage {
     pub test: String,
+}
+
+#[derive(Deserialize)]
+pub struct MenuItem {
+    pub SKU: i32,
+    pub name: String,
+    pub quantity: i32
+}
+
+#[derive(Serialize, FromRow)]
+pub struct ReceiptItem {
+    pub lobby_id: i32,
+    pub SKU: i32,
+    pub name: String,
+    pub quantity: i32
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Lobby {
+    lobby_id: i32
 }
 
 #[get("/api/get/messages")]
@@ -57,4 +78,61 @@ pub async fn clear_messages(state:Data<AppState>) -> impl Responder {
         Ok(_) => HttpResponse::Ok().json("Messages have been cleared"),
         Err(_) => HttpResponse::InternalServerError().json("Failed to clear messages"),
     }
+}
+
+#[post("/api/receipt/post_receipt")]
+pub async fn post_receipt(state:Data<AppState>, body: web::Json<Vec<MenuItem>>) -> impl Responder {
+    let lobby_number = futures::executor::block_on(create_new_lobby(state.clone()));
+    for MenuItem in body.into_inner() {
+        {
+            match sqlx::query!("INSERT INTO menu_item(SKU, name, quantity) VALUES($1,$2,$3)",MenuItem.SKU, MenuItem.name, MenuItem.quantity)
+                .execute(&state.db)
+                .await 
+                {
+                    Ok(_) => (),
+                    Err(_) => print!("Unable to insert menu item into table"),
+                };
+
+           
+            match sqlx::query!("INSERT INTO receipt_item(lobby_id, SKU) VALUES($1,$2)", lobby_number, MenuItem.SKU)
+                .execute(&state.db)
+                .await 
+                {
+                    Ok(_) => (),
+                    Err(_) => print!("Unable to insert receipt item into table"),
+                };
+        }
+    }
+    HttpResponse::Ok().json(lobby_number)
+}
+
+#[post("/api/lobby/join")]
+pub async fn join_lobby(state: Data<AppState>, body: Json<Lobby>) -> impl Responder {
+    let query_string = format!("SELECT lobby_id, SKU, name, quantity FROM receipt_item JOIN lobby USING(lobby_id) JOIN menu_item USING(sku) WHERE lobby_id = {}", body.lobby_id);
+    match sqlx::query_as!(
+        ReceiptItem,
+        "SELECT lobby_id, SKU AS \"SKU\", name, quantity FROM receipt_item JOIN lobby USING(lobby_id) JOIN menu_item USING(sku) WHERE lobby_id = $1",
+        body.lobby_id
+    )
+        .fetch_all(&state.db)
+        .await
+    {
+        Ok(data) => HttpResponse::Ok().json(data),
+        Err(_) => HttpResponse::NotFound().json( query_string),
+    }
+    
+}
+
+
+pub async fn create_new_lobby(state:Data<AppState>) -> i32 {
+    let mut rng = rand::thread_rng();
+    let lobby_number = rng.gen::<i32>();
+    match sqlx::query!("INSERT INTO lobby VALUES($1)",lobby_number)
+    .execute(&state.db)
+    .await
+    {
+        Ok(_) => lobby_number,
+        Err(_) => 0
+    };
+    lobby_number
 }
