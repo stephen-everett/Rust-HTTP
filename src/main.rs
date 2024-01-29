c7ccec2602b82879ac12318794fd276c1293aecd
Stephen Everett <alex.alexeverett@gmail.com>
2024-01-28 18:42:08 -0800

    commiting before merge

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,11 @@ pub struct AppState {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    /*
+        Create a connection pool to the PG database using env variable "DATABASE_URL". In dev environment
+        this is defined in a .env file in the root folder of the project. In production it's defined in an
+        actual environment variable. (Don't upload .env to github. Add to .gitignore)
+     */
     dotenv().ok();
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = PgPoolOptions::new()
@@ -19,6 +24,11 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("Error building a connection pool");
 
+
+    /*
+        Start the server and use defined endpoints. Endpoints are defined in services.rs
+        and imported at the top of this file
+     */
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(AppState { db: pool.clone() }))
