fba7828b251b4a1a58062866ef704cf013dd5f07
Stephen Everett <alex.alexeverett@gmail.com>
2024-02-17 16:35:58 -0800

    some more comments

--- src/routes/app/delete_user.rs
+++ src/routes/app/delete_user.rs
@@ -2,6 +2,10 @@ use actix_web::{get, web::{Data, ReqData}, Responder, HttpResponse};
 use crate::structs::app_state::{AppState, TokenClaims};
 
 /// Deletes user by extracting user_id from ReqData claims (JWT Token)
+/*
+    Author: Stephen Everett
+    Contributors: Khirby Calma(Front-end)
+*/
 #[get("/api/delete_user")]
 async fn delete_user(state: Data<AppState>, claims: Option<ReqData<TokenClaims>>) -> impl Responder {
     
