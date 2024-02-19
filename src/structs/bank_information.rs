3f78f4f27deefe179bfa105099244700afac4372
Lsobaca <61958964+Lsobaca@users.noreply.github.com>
2024-02-18 19:10:22 -0800

    added some things

--- src/structs/bank_information.rs
+++ src/structs/bank_information.rs
@@ -1,5 +1,6 @@
 use serde::{Serialize, Deserialize};
 use sqlx::FromRow;
+// use crate::structs::user::User;
 
 /// Struct to get the bank information for the user to send it to the server.
 /// userid is the primary key to find the the bank information on the user
@@ -11,3 +12,15 @@ struct BankInformation{
     bank_routing: String,
     bank_account_number:String
 }
+
+pub struct UserBank{
+    pub info: BankInformation
+}
+
+impl std::ops::Deref for UserBank{
+    type Target = BankInformation;
+    fn deref(&self) -> &Self::Target {
+        &self.info
+    }
+}
+
