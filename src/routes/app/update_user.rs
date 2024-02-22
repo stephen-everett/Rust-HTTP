80adeca240e9d225ee9c381c7e21682af15ea6a5
Lsobaca <Lsobaca01@gmail.com>
2024-02-22 14:35:27 -0800

    added the function names for the updates

--- src/routes/app/update_user.rs
+++ src/routes/app/update_user.rs
@@ -2,3 +2,52 @@ use actix_web::{post, web::{Data, ReqData}, Responder, HttpResponse};
 
 
 
+async fn update_first_name(state:Data<AppState>,token: Options<ReqData<TokenClaims>>) -> impl Responder{
+
+
+
+
+}
+
+
+async fn update_last_name(state:Data<AppState>,token: Options<ReqData<TokenClaims>>) -> impl Responder{
+
+
+    
+
+}
+
+
+
+async fn update_phone_number(state:Data<AppState>,token: Options<ReqData<TokenClaims>>) -> impl Responder{
+
+
+    
+
+}
+
+
+
+
+async fn update_email(state:Data<AppState>,token: Options<ReqData<TokenClaims>>) -> impl Responder{
+
+
+    
+
+}
+
+async fn update_pin(state:Data<AppState>,token: Options<ReqData<TokenClaims>>) -> impl Responder{
+
+
+    
+
+}
+
+async fn update_password(state:Data<AppState>,token: Options<ReqData<TokenClaims>>) -> impl Responder{
+
+
+    
+
+}
+
+
