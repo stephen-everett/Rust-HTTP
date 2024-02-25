2737a1b12eecd00d562c0518b2395a39b87d2122
Lsobaca <61958964+Lsobaca@users.noreply.github.com>
2024-02-24 18:36:54 -0800

    some formatting and a some comments

--- src/routes/app/add_bank.rs
+++ src/routes/app/add_bank.rs
@@ -1,9 +1,9 @@
 use actix_web::{post, web::{Data, ReqData}, Responder, HttpResponse};
-
+use crate::structs::app_state::{AppState, TokenClaims};
 
 
 #[post("/add_bank")]
-async fn add_bank(){
+async fn add_bank(state:Data<AppState>){
 
     
 }
--- src/routes/app/update_user.rs
+++ src/routes/app/update_user.rs
@@ -4,7 +4,8 @@ use crate::structs::{app_state::{AppState, TokenClaims},
 use argonautica::Hasher;
 use crate::routes::auth::register::{unique_phone,unique_email,unique_username};
 
-
+/// The user sends a new username to be updated on the database. The function is going to check if the username is already
+/// in the databasea. If it is not in the database, then the username would be changed in database if it is available.
 #[post("/username")]
 async fn update_username(state:Data<AppState>,token:Option<ReqData<TokenClaims>>,body:Json<Username>)->impl Responder{
     match token{
@@ -15,9 +16,9 @@ async fn update_username(state:Data<AppState>,token:Option<ReqData<TokenClaims>>
             else{
                 let up_query = "UPDATE users SET username = $1 WHERE user_id = $2";
                 match sqlx::query(up_query)
-                .bind(body.name.clone())
-                .bind(token.user_id.to_string())
-                .execute(&state.db)
+                    .bind(body.name.clone())
+                    .bind(token.user_id.to_string())
+                    .execute(&state.db)
                 .await{
                     Ok(user)=>HttpResponse::Ok().json("updated username"),
                     Err(err)=> HttpResponse::InternalServerError().json(format!("{:?}",err))
@@ -28,25 +29,27 @@ async fn update_username(state:Data<AppState>,token:Option<ReqData<TokenClaims>>
     }
 }
 
+/// Updates the first_name of the user
 #[post("/first_name")]
 async fn update_first_name(state:Data<AppState>,token: Option<ReqData<TokenClaims>>, body:Json<FirstName>) -> impl Responder{
     match token {
-    Some(token)=>{
+        Some(token)=>{
         //let name: FirstName = body.into_inner();
         let up_query = "UPDATE users SET first_name = $1 WHERE user_id = $2";
         match sqlx::query(up_query)
-        .bind(body.name.clone())
-        .bind(token.user_id.to_string())
-        .execute(&state.db)
+            .bind(body.name.clone())
+            .bind(token.user_id.to_string())
+            .execute(&state.db)
         .await{
             Ok(name)=> HttpResponse::Ok().json("name has been changed"),
             Err(err)=> HttpResponse::InternalServerError().json(format!("{:?}",err))
         }
-     },
+         },
      None => HttpResponse::InternalServerError().json("Something was wrong with token")
     }
 }
 
+/// Changes the last name of the user
 #[post("/last_name")]
 async fn update_last_name(state:Data<AppState>,token: Option<ReqData<TokenClaims>>,body:Json<LastName>) -> impl Responder{
     match token {
@@ -54,31 +57,32 @@ async fn update_last_name(state:Data<AppState>,token: Option<ReqData<TokenClaims
             //let name: FirstName = body.into_inner();
             let up_query = "UPDATE users SET last_name = $1 WHERE user_id = $2";
             match sqlx::query(up_query)
-            .bind(body.name.clone())
-            .bind(token.user_id.to_string())
-            .execute(&state.db)
+                .bind(body.name.clone())
+                .bind(token.user_id.to_string())
+                .execute(&state.db)
             .await{
                 Ok(name)=> HttpResponse::Ok().json("name has been changed"),
                 Err(err)=> HttpResponse::InternalServerError().json(format!("{:?}",err))
             }
          },
-         None => HttpResponse::InternalServerError().json("Something was wrong with token")
+        None => HttpResponse::InternalServerError().json("Something was wrong with token")
         }
 }
 
+/// Updates the phone_number of the user and checks if someone else already has the phone_number
 #[post("/phone_number")]
 async fn update_phone_number(state:Data<AppState>,token: Option<ReqData<TokenClaims>>,body:Json<UpdatePhoneNumber>) -> impl Responder{
     match token {
         Some(token)=>{
             //let name: FirstName = body.into_inner();
             if !unique_phone(state.clone(),body.number.clone()).await{
-                return  HttpResponse::Conflict().json("Not a unique Phone number");
+                return  HttpResponse::Conflict().json("Not a vaild Phone number");
             }
             else{let up_query = "UPDATE users SET phone_number = $1 WHERE user_id = $2";
                 match sqlx::query(up_query)
-                .bind(body.number.clone())
-                .bind(token.user_id.to_string())
-                .execute(&state.db)
+                    .bind(body.number.clone())
+                    .bind(token.user_id.to_string())
+                    .execute(&state.db)
                 .await{
                     Ok(name)=> HttpResponse::Ok().json("name has been changed"),
                     Err(err)=> HttpResponse::InternalServerError().json(format!("{:?}",err))
@@ -89,20 +93,21 @@ async fn update_phone_number(state:Data<AppState>,token: Option<ReqData<TokenCla
         }
 }
 
+/// Updates the email for the user and checks if the email is unique and free
 #[post("/email")]
 async fn update_email(state:Data<AppState>,token: Option<ReqData<TokenClaims>>,body:Json<UpdateEmail>) -> impl Responder{
     match token {
         Some(token)=>{
             //let name: FirstName = body.into_inner();
             if !unique_email(state.clone(), body.name.clone()).await{
-                return HttpResponse::Conflict().json("not unique email");
+                return HttpResponse::Conflict().json("not a vaild email");
             }
             else{
                 let up_query = "UPDATE users SET email = $1 WHERE user_id = $2";
                 match sqlx::query(up_query)
-                .bind(body.name.clone())
-                .bind(token.user_id.to_string())
-                .execute(&state.db)
+                    .bind(body.name.clone())
+                    .bind(token.user_id.to_string())
+                    .execute(&state.db)
                 .await{
                     Ok(name)=> HttpResponse::Ok().json("name has been changed"),
                     Err(err)=> HttpResponse::InternalServerError().json(format!("{:?}",err))
@@ -111,9 +116,10 @@ async fn update_email(state:Data<AppState>,token: Option<ReqData<TokenClaims>>,b
           
          },
          None => HttpResponse::InternalServerError().json("Something was wrong with token")
-        }
+    }
 }
 
+/// Updates the PIN 
 #[post("/pin")]
 async fn update_pin(state:Data<AppState>,token: Option<ReqData<TokenClaims>>,body:Json<UpdatePIN>) -> impl Responder{
     match token {
@@ -121,9 +127,9 @@ async fn update_pin(state:Data<AppState>,token: Option<ReqData<TokenClaims>>,bod
             //let name: FirstName = body.into_inner();
             let up_query = "UPDATE users SET pin = $1 WHERE user_id = $2";
             match sqlx::query(up_query)
-            .bind(body.pin.clone())
-            .bind(token.user_id.to_string())
-            .execute(&state.db)
+                .bind(body.pin.clone())
+                .bind(token.user_id.to_string())
+                .execute(&state.db)
             .await{
                 Ok(name)=> HttpResponse::Ok().json("name has been changed"),
                 Err(err)=> HttpResponse::InternalServerError().json(format!("{:?}",err))
@@ -133,6 +139,7 @@ async fn update_pin(state:Data<AppState>,token: Option<ReqData<TokenClaims>>,bod
     }
 }
 
+/// the user sends a password where it is going to be hashed to be stored in database
 #[post("/password")]
 async fn update_password(state:Data<AppState>,token: Option<ReqData<TokenClaims>>, body:Json<UpdatePassword>) -> impl Responder{
     match token {
@@ -142,20 +149,19 @@ async fn update_password(state:Data<AppState>,token: Option<ReqData<TokenClaims>
             let hash_secret = std::env::var("HASH_SECRET").expect("HASH_SECRET needs to be set!");
             let mut hasher = Hasher::default();
             let hash = hasher
-            .with_password(body.pass.clone())
-            .with_secret_key(hash_secret)
-            .hash()
-            .unwrap();
-            
+                .with_password(body.pass.clone())
+                .with_secret_key(hash_secret)
+                .hash()
+                .unwrap();
             match sqlx::query(up_query)
-            .bind(hash)
-            .bind(token.user_id.to_string())
-            .execute(&state.db)
+                .bind(hash)
+                .bind(token.user_id.to_string())
+                .execute(&state.db)
             .await{
                 Ok(name)=> HttpResponse::Ok().json("name has been changed"),
                 Err(err)=> HttpResponse::InternalServerError().json(format!("{:?}",err))
             }
          },
         None => HttpResponse::InternalServerError().json("Something was wrong with token")
-     }
+    }
 }
