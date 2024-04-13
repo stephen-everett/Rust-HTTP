16bb12ed1fef50e7b35cb346ac955819b7076d06
Lsobaca <61958964+Lsobaca@users.noreply.github.com>
2024-04-12 23:42:15 -0700

    slight change

--- src/routes/app/checker.rs
+++ src/routes/app/checker.rs
@@ -27,8 +27,8 @@ async fn is_password(state:Data<AppState>, token:Option<ReqData<TokenClaims>>,bo
                        // let incoming_password = Password{ pass: hash};
                         let mut verifier = Verifier::default();
                         let is_valid = verifier
-                                            .with_hash(password.pass)
-                                            .with_password(body.pass.clone())
+                                            .with_hash(password.password)
+                                            .with_password(body.password.clone())
                                             .with_secret_key(hash_secret)
                                             .verify()
                                             .unwrap();
